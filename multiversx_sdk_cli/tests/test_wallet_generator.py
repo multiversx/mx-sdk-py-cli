1d915e7faa64e70b344a7d4e255063135671c127
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-01-19 10:32:03 +0200

    use context for monkeypatch

--- multiversx_sdk_cli/tests/test_wallet_generator.py
+++ multiversx_sdk_cli/tests/test_wallet_generator.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any
 from multiversx_sdk_cli.cli import main
 
 def test_generate_wallet():
@@ -22,12 +23,14 @@ def test_generate_wallet_and_save_in_pem_format():
         assert False
 
 
-def test_derive_pem_from_mnemonic(monkeypatch):
+def test_derive_pem_from_mnemonic(monkeypatch: Any):
     output_path = Path(__file__).parent / "testdata-out" / "wallet.pem"
     test_mnemonic = "moral volcano peasant pass circle pen over picture flat shop clap goat never lyrics gather prepare woman film husband gravity behind test tiger improve"
-    monkeypatch.setattr('builtins.input', lambda _: test_mnemonic)
+    with monkeypatch.context() as mp:
+        mp.setattr('builtins.input', lambda _: test_mnemonic)
 
-    result = main(['wallet', 'derive', str(output_path), '--mnemonic'])
+        result = main(['wallet', 'derive', str(output_path), '--mnemonic'])
+    
     assert Path.is_file(output_path) == True
 
     if not result:
