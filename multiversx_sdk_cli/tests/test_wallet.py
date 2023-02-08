d16554c77a7ff45bca349e6c600ddaab972041fc
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-02-08 23:21:39 +0200

    Remove "pynacl" as a dependency (not needed anymore).

--- multiversx_sdk_cli/tests/test_wallet.py
+++ multiversx_sdk_cli/tests/test_wallet.py
@@ -1,6 +1,5 @@
 import logging
 
-import nacl.signing
 from multiversx_sdk_cli.accounts import Account
 from multiversx_sdk_cli.errors import GasLimitTooLarge
 from multiversx_sdk_cli.tests.utils import MyTestCase
@@ -14,20 +13,6 @@ class WalletTestCase(MyTestCase):
         super().setUp()
         self.alice = Account(pem_file=str(self.devnet_wallets.joinpath("users", "alice.pem")))
         self.multiple_bls_keys_file = self.testdata / 'multipleValidatorsKeys.pem'
-
-    def test_nacl_playground_signing(self):
-        secret_key_hex = "b8211b08edc8aca591bedf1b9aba47e4077e54ac7d4ceb2f1bc9e10c064d3e6c7a5679a427f6df7adf2310ddf5e570fd51e47e6b1511124d6b250b989b017588"
-        secret_key_bytes = bytes.fromhex(secret_key_hex)
-        secret_key_seed_bytes = secret_key_bytes[:32]
-        signing_key = nacl.signing.SigningKey(secret_key_seed_bytes)
-        signed = signing_key.sign(b"test")
-        signature = signed.signature
-        signed_bytes_hex = signature.hex()
-
-        self.assertEqual(
-            "a4918458d874ca58893a1f92dac33e7b10e3bf46048ad5de5bc260487ca84e8e07603297120fdc018242f63bd8e87b13efd108f8ffa095f536b6eda03805590c",
-            signed_bytes_hex)
-        self.assertEqual(64, len(signature))
 
     def test_sign_transaction(self):
         # With data
