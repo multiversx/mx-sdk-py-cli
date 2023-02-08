5b99ef2678843f25ced1c25edb7fb8e071a2d4ab
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-02-08 14:22:47 +0200

    Fix error removal.

--- multiversx_sdk_cli/errors.py
+++ multiversx_sdk_cli/errors.py
@@ -79,9 +79,9 @@ class BuildError(KnownError):
         super().__init__(f"Build error: {message}.", inner)
 
 
-class ProxyRequestError(KnownError):
-    def __init__(self, url: str, data: Any):
-        super().__init__(f"Proxy request error for url [{url}]: {data}")
+class UnknownArgumentFormat(KnownError):
+    def __init__(self, argument: Any):
+        super().__init__(f"Cannot handle non-hex, non-number arguments yet: {argument}.")
 
 
 class BadInputError(KnownError):
