9f0da5427c5fa0820b8f7685353ecf9c75ef1f96
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-04-24 12:15:44 +0300

    Use fixed version of Rust when running tests.

--- multiversx_sdk_cli/tests/test_cli_deps.sh
+++ multiversx_sdk_cli/tests/test_cli_deps.sh
@@ -5,12 +5,10 @@ source "./shared.sh"
 testAll() {
     set -x
 
-    ${CLI} --verbose deps install rust
-    ${CLI} --verbose deps install clang
+    ${CLI} --verbose deps install rust --tag=${VERSION_RUST}
     ${CLI} --verbose deps install vmtools --overwrite
 
     ${CLI} --verbose deps check rust
-    ${CLI} --verbose deps check clang
     ${CLI} --verbose deps check vmtools
 
     set +x
