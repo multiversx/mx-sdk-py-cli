f93f42e9e98419669e20599c71240852945f5713
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-05-22 15:53:54 +0300

    fix test

--- multiversx_sdk_cli/tests/test_sign.py
+++ multiversx_sdk_cli/tests/test_sign.py
@@ -7,6 +7,7 @@ def test_sign_tx():
     parent = Path(__file__).parent
     unsigned_transaction = parent / "testdata" / "transaction.json"
     signed_transaction = parent / "testdata-out" / "signed_transaction.json"
+    expected_signature = "7b0fa3bd477a9aacdfd8d6b41628e525afbbc94b4b56c2a30a10f78514c2f6558b27eef701633481f1ef54b62697c91e9dc06cc6d2038bd13cf9557467142005"
 
     main([
         "tx",
@@ -22,4 +23,4 @@ def test_sign_tx():
     with open(signed_transaction) as f:
         signed_tx = json.load(f)
 
-    assert signed_tx["emittedTransaction"]["signature"]
+    assert signed_tx["emittedTransaction"]["signature"] == expected_signature
