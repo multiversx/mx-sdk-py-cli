7ef887f4f64f5cf78b8cdebadcb11a8a6dd3a60f
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-09-18 15:53:20 +0300

    set flag for pytest logs

--- multiversx_sdk_cli/tests/test_cli_contracts.py
+++ multiversx_sdk_cli/tests/test_cli_contracts.py
@@ -38,7 +38,7 @@ def test_contract_new_with_bad_code():
 
 
 def replace_variable_with_unknown_variable():
-    # this is done in order to replace the value added in the adder contract witha unknown variable
+    # this is done in order to replace the value added in the adder contract with a unknown variable
     with open(parent / "testdata-out" / "SANDBOX" / "adder-bad-src" / "src" / "adder.rs", "r") as f:
         contract_lines = f.readlines()
 
