fc8609ac2afb3dd8db3b846f0ea0d80a83640fe6
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-09-27 19:03:15 +0300

    fix localnet and unit tests

--- multiversx_sdk_cli/tests/test_cli_deps.py
+++ multiversx_sdk_cli/tests/test_cli_deps.py
@@ -1,53 +1,45 @@
 import shutil
+from pathlib import Path
 
 import pytest
 
 from multiversx_sdk_cli.cli import main
-from multiversx_sdk_cli.config import get_dependency_tag
 
 
 def test_deps_install_rust():
-    default_tag = get_dependency_tag("rust")
-    return_code = main(["deps", "install", "rust", "--tag", default_tag, "--overwrite"])
-    if return_code:
-        assert False
-    else:
-        assert True
+    return_code = main(["deps", "install", "rust", "--overwrite"])
+    assert True if return_code == 0 else False
+
+
+def test_deps_check_rust():
+    return_code = main(["deps", "check", "rust"])
+    assert True if return_code == 0 else False
+
+    which_rustc = shutil.which("rustc")
+    if which_rustc:
+        assert Path.is_file(Path(which_rustc))
+
+    which_cargo = shutil.which("cargo")
+    if which_cargo:
+        assert Path.is_file(Path(which_cargo))
+
+    which_sc_meta = shutil.which("sc-meta")
+    if which_sc_meta:
+        assert Path.is_file(Path(which_sc_meta))
+
+    which_wasm_opt = shutil.which("wasm-opt")
+    if which_wasm_opt:
+        assert Path.is_file(Path(which_wasm_opt))
+
+    which_twiggy = shutil.which("twiggy")
+    if which_twiggy:
+        assert Path.is_file(Path(which_twiggy))
 
 
 @pytest.mark.skip_on_windows
 def test_deps_install_vmtools():
     return_code = main(["deps", "install", "vmtools"])
-    if return_code:
-        assert False
-    else:
-        assert True
-
-
-def test_deps_check_rust():
-    return_code = main(["deps", "check", "rust"])
-    if return_code:
-        assert False
-    else:
-        assert True
-
-
-@pytest.mark.skip_on_windows
-def test_check_sc_meta():
-    which_sc_meta = shutil.which("sc-meta")
-    if which_sc_meta:
-        assert True
-    elif which_sc_meta is None:
-        assert False
-
-
-@pytest.mark.skip_on_windows
-def test_check_wasm_opt():
-    which_wasm_opt = shutil.which("wasm-opt")
-    if which_wasm_opt:
-        assert True
-    elif which_wasm_opt is None:
-        assert False
+    assert True if return_code == 0 else False
 
 
 @pytest.mark.skip_on_windows
