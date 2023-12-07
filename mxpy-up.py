5ac8b35d9faec7c1afa68c06af34ba17342f9e59
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-12-07 14:56:26 +0200

    auto-update for windows (take 2)

--- mxpy-up.py
+++ mxpy-up.py
@@ -226,7 +226,9 @@ def get_mxpy_update_shortcut_content():
 
     if operating_system == "windows":
         return f"""#!/bin/sh
-curl.exe --output mxpy-up.py --url https://raw.githubusercontent.com/multiversx/mx-sdk-py-cli/mxpy-update/mxpy-up.py && python mxpy-up.py --not-interactive %*
+set "python_script=%~dp0mxpy-up.py"
+curl.exe --output "%python_script%" --url https://raw.githubusercontent.com/multiversx/mx-sdk-py-cli/mxpy-update/mxpy-up.py
+python "%python_script%" --not-interactive %*
 """
 
     return f"""#!/bin/sh
