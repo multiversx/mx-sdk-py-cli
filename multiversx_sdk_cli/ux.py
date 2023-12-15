f038b3df383ec6309107e9cdc6d6bff4eaab9e25
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-12-15 17:02:48 +0200

    escape message when printing using rich

--- multiversx_sdk_cli/ux.py
+++ multiversx_sdk_cli/ux.py
@@ -1,14 +1,15 @@
 from rich import print
+from rich.markup import escape
 from rich.panel import Panel
 
 
 def show_message(message: str):
-    print(Panel(f"[green]{message}"))
+    print(Panel(f"[green]{escape(message)}"))
 
 
 def show_critical_error(message: str):
-    print(Panel(f"[red]{message}"))
+    print(Panel(f"[red]{escape(message)}"))
 
 
 def show_warning(message: str):
-    print(Panel(f"[yellow]{message}"))
+    print(Panel(f"[yellow]{escape(message)}"))
