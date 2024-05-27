a1b09655759a06ae592511634e4db777a12fa49e
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-05-27 16:27:29 +0300

    Undo some changes.

--- multiversx_sdk_cli/localnet/node_config_toml.py
+++ multiversx_sdk_cli/localnet/node_config_toml.py
@@ -43,8 +43,28 @@ def patch_api(data: ConfigDict, config: ConfigRoot):
 
 
 def patch_enable_epochs(data: ConfigDict, config: ConfigRoot):
-    max_nodes_change_enable_epoch = data["EnableEpochs"]["MaxNodesChangeEnableEpoch"]
+    enable_epochs = data["EnableEpochs"]
+    enable_epochs['SCDeployEnableEpoch'] = 0
+    enable_epochs['BuiltInFunctionsEnableEpoch'] = 0
+    enable_epochs['RelayedTransactionsEnableEpoch'] = 0
+    enable_epochs['PenalizedTooMuchGasEnableEpoch'] = 0
+    enable_epochs['AheadOfTimeGasUsageEnableEpoch'] = 0
+    enable_epochs['GasPriceModifierEnableEpoch'] = 0
+    enable_epochs['RepairCallbackEnableEpoch'] = 0
+    enable_epochs['ReturnDataToLastTransferEnableEpoch'] = 0
+    enable_epochs['SenderInOutTransferEnableEpoch'] = 0
+    enable_epochs['ESDTEnableEpoch'] = 0
+    enable_epochs['IncrementSCRNonceInMultiTransferEnableEpoch'] = 0
+    enable_epochs['ESDTMultiTransferEnableEpoch'] = 0
+    enable_epochs['GlobalMintBurnDisableEpoch'] = 0
+    enable_epochs['ESDTTransferRoleEnableEpoch'] = 0
+    enable_epochs['BuiltInFunctionOnMetaEnableEpoch'] = 0
+    enable_epochs['MultiESDTTransferFixOnCallBackOnEnableEpoch'] = 0
+    enable_epochs['ESDTNFTCreateOnMultiShard'] = 0
+    enable_epochs['MetaESDTSetEnableEpoch'] = 0
+    enable_epochs['DelegationManagerEnableEpoch'] = 0
+
+    max_nodes_change_enable_epoch = enable_epochs['MaxNodesChangeEnableEpoch']
     last_entry = max_nodes_change_enable_epoch[-1]
     penultimate_entry = max_nodes_change_enable_epoch[-2]
-    last_entry["MaxNumNodes"] = penultimate_entry["MaxNumNodes"] - (config.shards.num_shards + 1) * penultimate_entry["NodesToShufflePerShard"]
-    
+    last_entry['MaxNumNodes'] = penultimate_entry['MaxNumNodes'] - (config.shards.num_shards + 1) * penultimate_entry['NodesToShufflePerShard']
