e8c89e1729c163b0ef57704ea2806a3607725aae
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-11-04 18:10:52 +0200

    clean remaining commented code.

--- multiversx_sdk_cli/tests/test_cli_validators.py
+++ multiversx_sdk_cli/tests/test_cli_validators.py
@@ -15,35 +15,6 @@ reward_address = "erd1k2s324ww2g0yj38qn2ch2jwctdy8mnfxep94q9arncc6xecg3xaq6mjse8
 bls_key = "e7beaa95b3877f47348df4dd1cb578a4f7cabf7a20bfeefe5cdd263878ff132b765e04fef6f40c93512b666c47ed7719b8902f6c922c04247989b7137e837cc81a62e54712471c97a2ddab75aa9c2f58f813ed4c0fa722bde0ab718bff382208"
 
 
-# @pytest.fixture()
-# def poll_endpoint():
-#     start_time = time.time()  # Record the start time
-#     timeout = 600
-#     interval = 1
-#
-#     while True:
-#         try:
-#             # Make the request to the endpoint
-#             response = requests.get(proxy_url, timeout=5)  # Add request timeout to prevent blocking indefinitely
-#             if response.status_code == 200:
-#                 # Break out of the loop if we get a successful response
-#                 return response.json()  # Return the response (or .text, .content based on your needs)
-#             else:
-#                 print(f"Received non-200 status code: {response.status_code}")
-#
-#         except requests.RequestException as e:
-#             # Handle network exceptions or timeouts
-#             print(f"Request failed: {e}")
-#
-#         # Check if the timeout is reached
-#         if time.time() - start_time > timeout:
-#             print("Polling timed out")
-#             break
-#
-#         # Wait for the specified interval before sending the next request
-#         time.sleep(interval)
-
-
 @pytest.mark.require_localnet
 def test_stake():
     validators_json = testdata_path / "validators_ci.json"
