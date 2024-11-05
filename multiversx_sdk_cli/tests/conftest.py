69404f006d0687bbd0971b0a39a653524ddb600a
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-11-05 10:14:37 +0200

    modified skip on windows marker.

--- multiversx_sdk_cli/tests/conftest.py
+++ multiversx_sdk_cli/tests/conftest.py
@@ -4,9 +4,9 @@ import pytest
 # function executed right after test items collected but before test run
 def pytest_collection_modifyitems(config, items):
     if not config.getoption('-m'):
-        skip_me = pytest.mark.skip(reason="to run marked tests, you need to explicitly run them wiht -m")
+        skip_me = pytest.mark.skip(reason="to run marked tests, you need to explicitly run them with -m")
         for item in items:
             if "require_localnet" in item.keywords:
                 item.add_marker(skip_me)
-            if "skip_on_winodws" in item.keywords:
+            if "run_on_windows" in item.keywords:
                 item.add_marker(skip_me)
