ca1d4fd9a178e7076ab19764d1155a4e955d2eda
Alexander Cristurean <alexander.cristurean@yahoo.com>
2024-11-06 10:27:22 +0200

    remove deleted shell scripts.

--- multiversx_sdk_cli/tests/test_cli_all.sh
+++ multiversx_sdk_cli/tests/test_cli_all.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 USE_PROXY=$1
 
+source ./shared.sh
+source ./test_cli_validators.sh && testAll
+
 testAll() {
     pushd $SCRIPT_DIR
     source ./shared.sh
