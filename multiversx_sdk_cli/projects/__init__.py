9c49436762383bc8a38f77ff78ef8de7eebff8cf
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-01-29 15:52:59 +0200

    drop contract report

--- multiversx_sdk_cli/projects/__init__.py
+++ multiversx_sdk_cli/projects/__init__.py
@@ -6,13 +6,11 @@ from multiversx_sdk_cli.projects.core import (
 )
 from multiversx_sdk_cli.projects.project_base import Project
 from multiversx_sdk_cli.projects.project_rust import ProjectRust
-from multiversx_sdk_cli.projects.report.do_report import do_report
 from multiversx_sdk_cli.projects.templates import Contract
 
 __all__ = [
     "build_project",
     "clean_project",
-    "do_report",
     "run_tests",
     "load_project",
     "Project",
